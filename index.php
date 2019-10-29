d40a756a77f5efc8a64e1769312003613418dce9
abilityMaster <makavoloka@gmail.com>
2019-10-29 14:09:35 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Кострома - Санкт-Петербург билеты, трансферы, индивидуальные заказы, багаж.
+                                        Ознакомиться с ценами, связаться
+                                        , забронировать место">
+    <meta name="keywords" content="Кострома - Санкт-Петербург билеты, Кострома - Санкт-Петербург трансфер, Кострома - Санкт-Петербург автобус">
+    <meta name="robots" content="all">
+    <link rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="/css/all.css" />
+    <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.structure.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.theme.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <title>Кострома - Санкт-петербург автобусом</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <div class="menu-top">
+            <div class="menu-top__container">
+                <div class="menu-top__logo">
+                    <div class="menu-top__logo-name">Пассажирские перевозки</div>
+                    <h1 class="main-header">Основное направление - Кострома - Санкт-Петербург на автобусе</h1>
+                </div>
+                <div class="menu-top__contacts flex-column">
+                    <div class="flex data">
+                        <div class="menu-top__phone flexable">
+                            <div class="menu-icon">
+                                <i class="fas fa-phone"></i>
+                            </div>
+                            <div>89536600007</div>
+                        </div>
+                        <div class="menu-top__mail flexable">
+                            <div class="menu-icon">
+                                <i class="fas fa-envelope-open"></i>
+                            </div>
+                            <div>kostroma-transport@google.com</div>
+                        </div>
+                    </div>
+                    <div class="flex menu-buttons">
+                        <div class="button button-order-phone popup-open">Заказать звонок</div>
+                        <div class="button button-what-pay">Узнать стоимость</div>
+                        <div class="button button-bron">Забронировать</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="block">
+            <nav class="menu">
+                <p><a href="#autopark">Наш автопарк</a></p>
+                <p><a href="">Услуги</a></p>
+                <p><a href="">Связаться с нами</a></p>
+                <p><a href="">Отзывы</a></p>
+            </nav>
+        </div>
+    </header>
+
+   <section class="main-background">
+       <div class="row-container">
+           <div class="info-block">
+               <div class="info-block-row info-block-viber">Viber - 8 920-999-23-23</div>
+               <div class="info-block-row info-block-whatsapp">Whatsapp - 8 920-999-23-23</div>
+           </div>
+           <div class="info-block info-block-right">
+               <div class="info-block-row info-block-vk">
+                   <a target="_blank" href="https://vk.com/vezem44">Наша группа Вконтакте</a>
+               </div>
+               <div class="info-block-row info-block-instagram">
+                   <a target="_blank" href="https://instagram.com/_vezem_">Наш instagram</a>
+               </div>
+           </div>
+       </div>
+       <div class="main-background-description">
+           Пассажирские перевозки по направлению<br>
+           <span class="direction-first">Кострома — <span class="city">Санкт-Петербург</span> — Кострома</span><br>
+           <span class="direction-second">Кострома — <span class="city">Москва</span> — Кострома</span>
+       </div>
+   </section>
+
+   <div class="container-divider"></div>
+
+    <div class="container">
+        <div class="pass-container">
+            <h2 class="h2">Преимущества работы с нами</h2>
+            <div class="flex advantages">
+                <div class="flex-column flex-vertical-center">
+                    <i class="advantages-icon material-icons">
+                        people
+                    </i>
+                    <div>Опытные водители</div>
+                </div>
+                <div class="flex-column flex-vertical-center">
+                    <i class="advantages-icon material-icons">
+                        av_timer
+                    </i>
+                    <div>Точность</div>
+                </div>
+                <div class="flex-column flex-vertical-center">
+                    <i class="advantages-icon material-icons">
+                        directions_bus
+                    </i>
+                    <div>Комфортное авто</div>
+                </div>
+            </div>
+        </div>
+        <h2 id="directions" class="h2">Направления и стоимость</h2>
+        <table class="rounded">
+            <thead>
+                <tr>
+                    <th>Направление</th>
+                    <th>Продолжительность в пути</th>
+                    <th>Стоимость</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Кострома - Санкт-Петербург</td>
+                    <td>7 часов</td>
+                    <td>2500 руб.</td>
+                </tr>
+                <tr>
+                    <td>Санкт-Петербург - Кострома</td>
+                    <td>7 часов</td>
+                    <td>2500 руб.</td>
+                </tr>
+                <tr>
+                    <td>Кострома - Москва</td>
+                    <td>7 часов</td>
+                    <td>1200 руб.</td>
+                </tr>
+                <tr>
+                    <td>Москва - Кострома</td>
+                    <td>7 часов</td>
+                    <td>1200 руб.</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="description">
+            <ul class="border">
+                <li>Возможен индивидуальный заказ</li>
+                <li>Багаж от 100 руб. Кошки Собаки, Документы, грузы</li>
+                <li>Заказ Машины в любом направлении 15 руб/км</li>
+            </ul>
+            <p class="p-link">Рассчитать стоимость</p>
+        </div>
+
+        <div class="container-divider-img"></div>
+<!--        <h2 class="h2">Выезд из Кострмы 3 раза в неделю</h2>-->
+<!--        <p>Место отправления в Костроме: Октябрьская площадь</p>-->
+<!--        <div>Забронировать место</div>-->
+        <div class="pass-container">
+            <h2 id="autopark" class="h2">Наш автопарк</h2>
+            <div class="owl-carousel owl-theme owl-loaded">
+                <div class="owl-stage-outer">
+                    <div class="owl-stage">
+                        <div class="owl-item slider-image slider-1">
+                            <div class="slider-description"><b>Volkswagen Caravella</b> - надежный немецкий автомобиль</div>
+                        </div>
+                        <div class="owl-item slider-image slider-2">
+                            <div class="slider-description"><b>Toyota Alphard</b> - воплощение роскоши и комфорта</div>
+                        </div>
+                        <div class="owl-item slider-image slider-3">
+                            <div class="slider-description"><b>Audi a6</b> - надежный немецкий автомобиль</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="pass-container">
+        <div class="inner-container">
+            <Form class="pass-form">
+                <h2 id="bronirovanie" style="color: white" class="h2">Забронировать поездку онлайн</h2>
+                <div class="flex pass-form-controls">
+                    <div class="flex-column">
+                        <label for="direction">Выберите направление</label>
+                        <select name="direction" id="direction">
+                            <option sum="" disabled selected>Выберите количество мест</option>
+                            <option sum="2500">Кострома — Санкт-Петербург</option>
+                            <option sum="2500">Санкт-Петербург — Кострома</option>
+                            <option sum="1200">Кострома — Москва</option>
+                            <option sum="1200">Москва — Кострома</option>
+                            <option sum="phoneMe">Другое, перезвонить мне</option>
+                        </select>
+                        <label for="time">Выберите время</label>
+                        <select name="time" id="time">
+                            <option time="" disabled selected>Выберите время отправления</option>
+                            <option time="time">19:00</option>
+                            <option time="time">19:00</option>
+                        </select>
+                    </div>
+                    <div class="flex-column">
+                        <label for="count">Количество мест</label>
+                        <select name="count" id="count">
+                            <option count="" disabled selected>Выберите количество мест</option>
+                            <option count="1">1</option>
+                            <option count="2">2</option>
+                            <option count="3">3</option>
+                            <option count="4">4</option>
+                            <option count="5">5</option>
+                            <option count="6">6</option>
+                        </select>
+                        <label for="datepicker">Выберите дату поездки:</label>
+                        <input type="text" id="datepicker">
+                    </div>
+                    <div class="flex-column">
+                        <label>Итоговая сумма:</label>
+                        <div class="flex flex-vertical-center flex-content-start">
+                            <i class="pass-form-logo-pay material-icons">monetization_on</i>
+                            <div id="sum"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex pass-form-submit">
+                    <div id="how-much-pay">Узнать стоимость</div>
+                </div>
+            </Form>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="pass-container about-us">
+            <h2 class="h2">О нас</h2>
+            <div class="flexable">
+                <div>
+                    <i class="material-icons">info</i>
+                </div>
+                <div class="about-us-right">
+                    <p>Работаем с 2011 года. Имеем в штате опытных водителей,
+                        произвели множество поездок с хорошими отзывами. В автопарке надежные автомобили.
+                        Осуществляем пассажирские перевозки по России, от 1 до 50 мест. Индивидуальный подход к каждому клиенту</p>
+
+                    <p>Остались вопросы звоните, наши контакты: </p>
+                    <div class="about-us-contacts">
+                        <div class="info-block-row info-block-viber">Viber - 8 920-999-23-23</div>
+                        <div class="info-block-row info-block-whatsapp">Whatsapp - 8 920-999-23-23</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="popup-fade">
+        <div class="popup">
+            <a class="popup-close" href="#">✕</a>
+            <p>Введите ваши данные и наш диспетчер перезвонит вам</p>
+            <form action="send.php" method="post">
+                <div class="flex-column">
+                    <label for="popup-name">Имя</label>
+                    <input id="popup-name" type="text" name="popup-name">
+                </div>
+                <div class="flex-column">
+                    <label for="popup-phone">Телефон</label>
+                    <input id="popup-phone" type="text" name="popup-phone">
+                </div>
+                <input class="popup-submit" type="submit" value="Отправить" />
+            </form>
+        </div>
+    </div>
+    <footer class="footer">
+        © 2019, Перевозки Кострома-Санкт-Петербург
+     </footer>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.maskedinput.min.js" type="text/javascript"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
