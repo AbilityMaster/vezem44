fa74e3641c0aebf7b0521903bbc9244e8c88acb3
abilityMaster <makavoloka@gmail.com>
2020-12-14 19:39:42 +0300

    changed logic of calculator: - removed old fields; - added new fields; - adaptive layout.

--- app/js/script.js
+++ app/js/script.js
@@ -9,29 +9,89 @@ $(document).ready(function() {
         }
     });
 
+
+    const kos = {
+        text: "Кострома",
+        value: "kos"
+    };
+    const snt = {
+        text: "Санкт-Петербург",
+        value: "snt"
+    };
+    const yar =  {
+        text: "Ярославль",
+        value: "yar"
+    };
+    const vol = {
+        text: "Вологда",
+        value: "vol"
+    };
+    const cher = {
+        text: "Череповец",
+        value: "cher"
+    };
+
+    const fromOptions = [
+        kos,
+        snt,
+        yar,
+        vol,
+        cher
+    ];
+    
+    const amountByDirection = {
+        ["kos-snt"]: 2000,
+        ["snt-kos"]: 2000,
+        ["kos-yar"]: 250,
+        ["kos-vol"]: 600,
+        ["kos-cher"]: 1200,
+        ["snt-cher"]: 1000,
+        ["snt-vol"]: 1500,
+        ["snt-yar"]: 2000,
+        ["yar-vol"]: 500,
+        ["yar-kos"]: 250,
+        ["yar-snt"]: 2000,
+        ["yar-cher"]: 1200,
+        ["vol-kos"]: 600,
+        ["vol-snt"]: 1500,
+        ["vol-yar"]: 500,
+        ["vol-cher"]: 600,
+        ["cher-snt"]: 1000,
+        ["cher-vol"]: 600,
+        ["cher-yar"]: 1200,
+        ["cher-kos"]: 1200,
+    }; 
     const pricesDict = new Map([
-        [1, {amount: 2000, time: "19:00"}],
-        [2, {amount: 2000, time: "19:00"}],
-        [3, {amount: 250, time: "19:00"}],
-        [4, {amount: 600, time: "19:00"}],
-        [5, {amount: 1200, time: "19:00"}],
-        [6, {amount: 1000, time: "19:00"}],
-        [7, {amount: 1500, time: "19:00"}],
-        [8, {amount: 2000, time: "19:00"}],
-        [9, {amount: 500, time: "20:30"}],
-        [10, {amount: 250, time: "5:00"}],
-        [11, {amount: 2000, time: "20:30"}],
-        [12, {amount: 1200, time: "20:30"}],
-        [13, {amount: 600, time: "2:00"}],
-        [14, {amount: 1500, time: "0:00"}],
-        [15, {amount: 500, time: "2:00"}],
-        [16, {amount: 600, time: "0:00"}],
-        [17, {amount: 1000, time: "2:00"}],
-        [18, {amount: 600, time: "0:30"}],
-        [19, {amount: 1200, time: "0:30"}],
-        [20, {amount: 1200, time: "0:30"}],
+            [1, {amount: 2000, time: "19:00"}],
+            [2, {amount: 2000, time: "19:00"}],
+            [3, {amount: 250, time: "19:00"}],
+            [4, {amount: 600, time: "19:00"}],
+            [5, {amount: 1200, time: "19:00"}],
+            [6, {amount: 1000, time: "19:00"}],
+            [7, {amount: 1500, time: "19:00"}],
+            [8, {amount: 2000, time: "19:00"}],
+            [9, {amount: 500, time: "20:30"}],
+            [10, {amount: 250, time: "5:00"}],
+            [11, {amount: 2000, time: "20:30"}],
+            [12, {amount: 1200, time: "20:30"}],
+            [13, {amount: 600, time: "2:00"}],
+            [14, {amount: 1500, time: "0:00"}],
+            [15, {amount: 500, time: "2:00"}],
+            [16, {amount: 600, time: "0:00"}],
+            [17, {amount: 1000, time: "2:00"}],
+            [18, {amount: 600, time: "0:30"}],
+            [19, {amount: 1200, time: "0:30"}],
+            [20, {amount: 1200, time: "0:30"}],
         ]
     );
+
+    const availableToByFrom = {
+        kos: [snt, yar, vol, cher],
+        snt: [kos, yar, vol, cher],
+        yar: [kos, snt, vol, cher],
+        vol: [kos, snt, yar, cher],
+        cher: [kos, snt, yar, vol],
+    };
 
     let form = {
         count: "",
@@ -39,45 +99,101 @@ $(document).ready(function() {
         date: "",
         time: ""
     };
+    
+    const isValid = () => form.to && form.from && form.count;
+    const calculate = () => {
+        const nameField = `${form.from}-${form.to}`;
 
-    let isSuccessCalculated = false;
-    const isValid = () => form.direction && form.count && form.date && form.time;
-    const calculate = () => { isSuccessCalculated = true; return `${form.direction * form.count} р.`; };
+        return `${amountByDirection[nameField] * form.count} р.`;
+    };
     const $sumBlock = $("#sum")[0];
     const $submitButton = $(".reservation__pass-form-submit-value")[0];
 
-    $("#direction")
+    // $("#direction")
+    //     .selectmenu({
+    //         appendTo: ".reservation__pass-form-column",
+    //         change: function(event, ui) {
+    //             const {item: {index}} = ui;
+    //             const {amount, time} = pricesDict.get(index) || {};
+    //
+    //             const select = document.querySelector('#time');
+    //
+    //             const today = new Date();
+    //
+    //             const timeNow = +`${today.getHours()}${today.getMinutes()}`;
+    //             const timeDirection = +time.replace(":", "");
+    //
+    //             if (timeNow > timeDirection) {
+    //                 form.time = form.time && "";
+    //
+    //                 const today = new Date();
+    //                 const dayInMs = 1000 * 60 * 60 * 24;
+    //
+    //                 today.setHours(0);
+    //                 today.setMinutes(0);
+    //                 today.setSeconds(0);
+    //                 today.setMilliseconds(0);
+    //
+    //                 const minDate = new Date(today.getTime() + dayInMs);
+    //
+    //                 $("#datepicker").datepicker( "option", "minDate", minDate);
+    //             } else {
+    //                 $("#datepicker").datepicker( "option", "minDate", new Date());
+    //             }
+    //
+    //             form.time = time;
+    //             select.options[1].text = time;
+    //             select.options[1].value = time;
+    //             select.value = time;
+    //
+    //             $( "#time" ).selectmenu( "refresh" );
+    //
+    //             form.direction = amount;
+    //
+    //             $("#direction-button").css({"background":"white"});
+    //
+    //             if (isValid()) {
+    //                 $sumBlock.innerHTML = calculate();
+    //             }
+    //         }
+    //     })
+    //     .selectmenu("menuWidget")
+    //     .addClass("overflow");
+
+    $("#from")
         .selectmenu({
-            appendTo: ".reservation__pass-form-column",
+            create: function(event, ui) {
+                const selectFrom = document.querySelector('#from');
+
+                fromOptions.forEach(({text, value}, index) => {
+                    selectFrom.options[index + 1] = new Option(text, value);
+                });
+
+                $("#from").selectmenu("refresh");
+            },
             change: function(event, ui) {
-                const {item: {index}} = ui;
-                const {amount, time} = pricesDict.get(index) || {};
+                form.from = ui.item.value;
 
-                const select = document.querySelector('#time');
+                $("#to").selectmenu( "option", "disabled", false );
 
-                form.time = time;
-                select.options[1].text = time;
-                select.options[1].value = time;
-                select.value = time;
+                const selectFrom = document.querySelector('#from');
 
-                $( "#time" ).selectmenu( "refresh" );
+                fromOptions.forEach(({text, value}, index) => {
+                    const isSelected = value === ui.item.value;
 
-                form.direction = amount;
+                    selectFrom.options[index + 1] = new Option(text, value, isSelected, isSelected);
+                    $("#from").selectmenu("refresh");
+                });
 
-                $("#direction-button").css({"background":"white"});
+                const selectTo = document.querySelector('#to');
 
-                if (isValid()) {
-                    $sumBlock.innerHTML = calculate();
-                }
-            }
-        })
-        .selectmenu("menuWidget")
-        .addClass("overflow");
+                availableToByFrom[ui.item.value].forEach(({text, value}, index) => {
+                    selectTo.options[index + 1] = new Option(text, value);
 
-    $("#time")
-        .selectmenu({
-            change: function(event, ui) {
-                form.time = ui.item.value;
+                    $("#to").selectmenu("refresh");
+                });
+
+
 
                 $("#time-button").css({"background":"white"});
 
@@ -89,8 +205,47 @@ $(document).ready(function() {
         })
         .selectmenu( "menuWidget" );
 
+
+    $("#to")
+        .selectmenu({
+            create: function(event, ui) {
+                $("#to").selectmenu( "option", "disabled", true );
+            },
+            change: function(event, ui) {
+                form.to = ui.item.value;
+
+                $("#count").selectmenu( "option", "disabled", false);
+
+                $("#time-button").css({"background":"white"});
+
+                if (isValid()) {
+                    $sumBlock.innerHTML = calculate();
+                }
+            }
+        })
+        .selectmenu( "menuWidget" );
+
+
+    // $("#time")
+    //     .selectmenu({
+    //         change: function(event, ui) {
+    //             form.time = ui.item.value;
+    //
+    //             $("#time-button").css({"background":"white"});
+    //
+    //             if (isValid()) {
+    //                 $sumBlock.innerHTML = calculate();
+    //             }
+    //
+    //         }
+    //     })
+    //     .selectmenu( "menuWidget" );
+
     $("#count")
         .selectmenu({
+            create: function(event, ui) {
+                $("#count").selectmenu( "option", "disabled", true);
+            },
             change: function() {
                 form.count = +$(this).find("option:selected")[0].getAttribute("count");
 
@@ -104,37 +259,38 @@ $(document).ready(function() {
         .selectmenu( "menuWidget" );
 
 
-    $("#datepicker").datepicker({
-        onSelect: function(dateText) {
-            form.date = dateText;
+    // $("#datepicker").datepicker({
+    //     minDate: new Date(),
+    //     onSelect: function(dateText) {
+    //         form.date = dateText;
+    //
+    //         $("#datepicker").css({"background":"white"});
+    //
+    //         if (isValid()) {
+    //             $sumBlock.innerHTML = calculate();
+    //         }
+    //     }
+    // });
 
-            $("#datepicker").css({"background":"white"});
-
-            if (isValid()) {
-                $sumBlock.innerHTML = calculate();
-            }
-        }
-    });
-
-    $.datepicker.regional.ru = {
-        closeText: "Закрыть",
-        prevText: "&#x3C;Пред",
-        nextText: "След&#x3E;",
-        currentText: "Сегодня",
-        monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
-            "Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
-        monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
-            "Июл","Авг","Сен","Окт","Ноя","Дек" ],
-        dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
-        dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
-        dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
-        weekHeader: "Нед",
-        dateFormat: "dd.mm.yy",
-        firstDay: 1,
-        isRTL: false,
-        showMonthAfterYear: false,
-        yearSuffix: "" };
-    $.datepicker.setDefaults( $.datepicker.regional.ru );
+    // $.datepicker.regional.ru = {
+    //     closeText: "Закрыть",
+    //     prevText: "&#x3C;Пред",
+    //     nextText: "След&#x3E;",
+    //     currentText: "Сегодня",
+    //     monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
+    //         "Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+    //     monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
+    //         "Июл","Авг","Сен","Окт","Ноя","Дек" ],
+    //     dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
+    //     dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
+    //     dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+    //     weekHeader: "Нед",
+    //     dateFormat: "dd.mm.yy",
+    //     firstDay: 1,
+    //     isRTL: false,
+    //     showMonthAfterYear: false,
+    //     yearSuffix: "" };
+    // $.datepicker.setDefaults( $.datepicker.regional.ru );
 
     $submitButton.addEventListener("click", () => {
         if (isValid()) {
